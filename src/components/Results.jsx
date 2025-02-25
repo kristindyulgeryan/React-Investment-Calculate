0545d2b08d02e7fe02450dd61c534eec57b14b59
Kristin <kristindyulgeryan@abv.bg>
2025-02-25 14:23:08 +0200

    Add outputting results in a list

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,8 +1,41 @@
-import { calculateInvestmentResults } from "../util/investment.js"
+import { calculateInvestmentResults, formatter } from "../util/investment.js"
 
 export default function Results({input}){
     const resultsData=calculateInvestmentResults(input)
-    console.log(resultsData);
+    const initialInvestment =
+    resultsData[0].valueEndOfYear 
+    - resultsData[0].interest 
+    - resultsData[0].annualInvestment
     
-    return<p> Results...</p>
+    return <table id="result">
+<thead>
+    <tr>
+        <th>Year</th>
+        <th>Investment Value</th>
+        <th>Interest (Year)</th>
+        <th> Total Interest </th>
+        <th> Interested Capital </th>
+    </tr>
+</thead>
+<tbody>
+   {resultsData.map(yearData=>{
+const totalInterest=
+yearData.valueEndOfYear 
+- yearData.annualInvestment 
+* yearData.year
+- initialInvestment;
+
+const totalAmountInvestment = yearData.valueEndOfYear - totalInterest;
+
+
+    return<tr key={yearData.year}>
+        <td>{yearData.year}</td>
+        <td>{formatter.format(yearData.valueEndOfYear)}</td>
+        <td>{formatter.format(yearData.interest)}</td>
+        <td>{formatter.format(totalInterest)}</td>
+        <td>{formatter.format(totalAmountInvestment)}</td>
+    </tr>
+   })} 
+</tbody>
+    </table>
 }
