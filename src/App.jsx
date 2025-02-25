788f3ececdef90e4ef289483ec8abe4e735111ad
Kristin <kristindyulgeryan@abv.bg>
2025-02-25 14:30:19 +0200

    Add conditional output for duration input

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ function App() {
     duration: 10,
 });
 
+const inputIsValid = userInput.duration >= 1;
+
 function handleChange(inputIdentifier,newValue){
   setUserInput(prevUserInput=>{
       return{
@@ -25,7 +27,9 @@ function handleChange(inputIdentifier,newValue){
     <>
     < Header/>
     <UseInput userInput={userInput} onChange={handleChange}/>
-    <Results input={userInput}/>
+
+{!inputIsValid && <p className="center">Please enter a duration greater than qero</p>}
+   {inputIsValid &&  <Results input={userInput}/>}
     </>
   )
 }
