8384f4adabee2c660c8254572423a1cd6829bc38
Kristin <kristindyulgeryan@abv.bg>
2025-02-25 13:42:35 +0200

    Add investment table with fields for initial investment, annual investment, expected investment, and duration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./components/Header.jsx"
+import UseInput from "./components/UserInput.jsx"
 
 function App() {
   return (
+    <>
     < Header/>
+    <UseInput/>
+    </>
   )
 }
 
